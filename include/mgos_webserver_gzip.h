1c5d362381b0108c9a806ee2c4a77504a2b84b0c
Holland <Dan.Holland@ricardo.com>
2019-06-12 15:55:47 +0100

    licence

--- include/mgos_webserver_gzip.h
+++ include/mgos_webserver_gzip.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Myles McNamara <https://smyl.es>
+ * Copyright (c) 2019 Dan Holland
  * All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the ""License"");
